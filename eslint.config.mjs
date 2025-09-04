2e99fcae88a9bf3ce88b51d05c0f639adc7ae4f9
ShyraiDan <dan.shirayy@gmail.com>
2025-09-04 23:44:22 +0300

    Feat: Setup basic eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,106 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import prettierPlugin from 'eslint-plugin-prettier'
+import tsParser from '@typescript-eslint/parser'
+import unusedImportsPlugin from 'eslint-plugin-unused-imports'
+import reactRefreshPlugin from 'eslint-plugin-react-refresh'
+import tanstackQueryPlugin from '@tanstack/eslint-plugin-query'
+import importSortPlugin from 'eslint-plugin-import'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+  recommendedConfig: {
+    env: {
+      browser: true,
+      es2021: true
+    }
+  }
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  ...compat.extends(
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'next/typescript',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:import/recommended'
+  ),
 
-export default eslintConfig;
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname
+      }
+    },
+    plugins: {
+      prettier: prettierPlugin,
+      'unused-imports': unusedImportsPlugin,
+      'react-refresh': reactRefreshPlugin,
+      '@tanstack/query': tanstackQueryPlugin,
+      import: importSortPlugin
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true
+        }
+      ],
+      '@typescript-eslint/prefer-nullish-coalescing': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+          singleQuote: true,
+          jsxSingleQuote: true,
+          semi: false,
+          trailingComma: 'none',
+          printWidth: 120,
+          bracketSameLine: true
+        }
+      ],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'jsx-quotes': ['error', 'prefer-single'],
+      'quote-props': ['error', 'as-needed'],
+      semi: ['error', 'never'],
+      'comma-dangle': ['error', 'never'],
+      'max-len': 'off',
+      '@tanstack/query/exhaustive-deps': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'object', 'type'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true }
+        }
+      ]
+    },
+    settings: {
+      'import/resolver': {
+        node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] }
+      }
+    }
+  }
+]
