6f97ef8344bfb4799c4cd1c09fa0987bdd7c3f22
ShyraiDan <dan.shirayy@gmail.com>
2025-09-07 13:53:57 +0300

    Feat: Created not found page

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -12,7 +12,9 @@ const buttonVariants = cva(
         default: 'bg-blue-300 text-white hover:bg-blue-200 hover:text-white',
         outline:
           'bg-transparent border border-solid border-blue-300 text-blue-300 hover:text-blue-200 hover:border-blue-200',
-        icon: 'bg-blue-300 p-3 rounded-2xl w-10 h-10 hover:bg-blue-200'
+        icon: 'bg-blue-300 p-3 rounded-2xl w-10 h-10 hover:bg-blue-200',
+        'outline-blue':
+          'tracking-[2px] px-5 py-3 uppercase font-bold font-primary text-blue-100 text-xs border border-solid border-blue-100 hover:text-white hover:bg-blue-100'
       }
     },
     defaultVariants: {
