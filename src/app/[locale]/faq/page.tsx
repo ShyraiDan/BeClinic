805e3219deec07229bc199c85f48c722d8a40e27
ShyraiDan <dan.shirayy@gmail.com>
2025-09-07 18:14:05 +0300

    Feat: Created Faq page

---
+++ src/app/[locale]/faq/page.tsx
@@ -0,0 +1,28 @@
+import { PageHeading } from '@/components/PageHeading/PageHeading'
+import { Container } from '@/components/ui/container'
+import { H1, P } from '@/components/ui/typography'
+import React from 'react'
+import { FaqItem } from '@/components/FaqItem/FaqItem'
+import { mockedFaqs } from '@/mocks/Faqs.mock'
+import { useTranslations } from 'next-intl'
+
+const FaqPage = () => {
+  const t = useTranslations('page')
+
+  return (
+    <>
+      <PageHeading title={t('faqs.title')} />
+      <Container>
+        <div className='max-w-[945px] mx-auto'>
+          <H1 className='mb-5 text-center'>{t('faqs.subtitle')}</H1>
+          <P className='text-center'>{t('faqs.description')}</P>
+        </div>
+        {mockedFaqs.map((item, i) => {
+          return <FaqItem key={i} item={item} />
+        })}
+      </Container>
+    </>
+  )
+}
+
+export default FaqPage
---
+++ src/mocks/Faqs.mock.ts
@@ -0,0 +1,38 @@
+export const mockedFaqs = [
+  {
+    question: 'faqs.questions.provideDentalCare.question',
+    answer: 'faqs.questions.provideDentalCare.answer'
+  },
+  {
+    question: 'faqs.questions.firstAppointment.question',
+    answer: 'faqs.questions.firstAppointment.answer'
+  },
+  {
+    question: 'faqs.questions.whoPerformsTreatment.question',
+    answer: 'faqs.questions.whoPerformsTreatment.answer'
+  },
+  {
+    question: 'faqs.questions.whyVisitImportant.question',
+    answer: 'faqs.questions.whyVisitImportant.answer',
+    listTitle: 'faqs.questions.whyVisitImportant.listTitle',
+    list: [
+      'faqs.questions.whyVisitImportant.list.preventCheckups',
+      'faqs.questions.whyVisitImportant.list.regularDiagnostics',
+      'faqs.questions.whyVisitImportant.list.byMaintaining',
+      'faqs.questions.whyVisitImportant.list.timelyVaccinations',
+      'faqs.questions.whyVisitImportant.list.regularContact'
+    ]
+  },
+  {
+    question: 'faqs.questions.whyVisitDentistImportant.question',
+    answer: 'faqs.questions.whyVisitDentistImportant.answer'
+  },
+  {
+    question: 'faqs.questions.whatAgeShouldVisitDentist.question',
+    answer: 'faqs.questions.whatAgeShouldVisitDentist.answer'
+  },
+  {
+    question: 'faqs.questions.whenShouldChangeToothbrush.question',
+    answer: 'faqs.questions.whenShouldChangeToothbrush.answer'
+  }
+]
