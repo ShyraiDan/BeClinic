7cf5249e06ce67aaed9aa8d9220f65bcb6a3c018
ShyraiDan <dan.shirayy@gmail.com>
2025-10-04 10:39:38 +0300

    Fix: Removed add blog button for patients and guests

--- src/components/BlogFilters/BlogFilters.tsx
+++ src/components/BlogFilters/BlogFilters.tsx
@@ -1,15 +1,18 @@
 import { Plus } from 'lucide-react'
-import { useTranslations } from 'next-intl'
+import { getTranslations } from 'next-intl/server'
 
+import { auth } from '@/auth'
 import { StyledLinkButton } from '@/components/ui/styledLinkButton'
 import { P } from '@/components/ui/typography'
+import { UserRoles } from '@/shared/types'
 
 interface BlogFiltersProps {
   length: number
 }
 
-export const BlogFilters = ({ length }: BlogFiltersProps) => {
-  const t = useTranslations('page')
+export const BlogFilters = async ({ length }: BlogFiltersProps) => {
+  const t = await getTranslations('page')
+  const session = await auth()
 
   return (
     <>
@@ -18,9 +21,11 @@ export const BlogFilters = ({ length }: BlogFiltersProps) => {
           {length > 0 && <P className='font-light text-black-300'>{t('blog.findPosts', { count: length })}</P>}
         </div>
         <div className='flex gap-4'>
-          <StyledLinkButton href='/blog/add' variant='icon' className='p-2.5 bg-blue-300'>
-            <Plus fill='#fff' size={16} />
-          </StyledLinkButton>
+          {session?.user.role === UserRoles.DOCTOR && (
+            <StyledLinkButton href='/blog/add' variant='icon' className='p-2.5 bg-blue-300'>
+              <Plus fill='#fff' size={16} />
+            </StyledLinkButton>
+          )}
         </div>
       </div>
     </>
